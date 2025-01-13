46cc8a6d16faab56696a3c70bbcc7de03a6579ed
Dev Bachani <dev22603@gmail.com>
2025-01-13 15:49:46 +0530

    prod management

--- Backend/app.mjs
+++ Backend/app.mjs
@@ -1,24 +1,26 @@
 // app.js (App Setup)
-import express from 'express';
-import bodyParser from 'body-parser';
-import authRoutes from './routes/user.routes.mjs';
-import productRoutes from './routes/product.routes.mjs';
-import adminRoutes from './routes/admin.user.routes.mjs';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/user.routes.mjs";
+import productRoutes from "./routes/product.routes.mjs";
+import adminRoutes from "./routes/admin.user.routes.mjs";
+import cors from "cors";
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow this frontend origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow credentials if needed
-  }));
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Allow this frontend origin
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true, // Allow credentials if needed
+    })
+);
 // Middleware
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/admin", adminRoutes);
 
-
-export {app};  // Export the app for use in index.js
+export { app }; // Export the app for use in index.js
 // export default app;  // Export the app for use in index.js
