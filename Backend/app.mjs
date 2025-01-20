5dedb6aede5913ec5e294abb0358d251f9802c8f
Dev Bachani <dev22603@gmail.com>
2025-01-20 05:52:26 +0530

    images handled

--- Backend/app.mjs
+++ Backend/app.mjs
@@ -7,13 +7,15 @@ import adminRoutes from "./routes/admin.user.routes.mjs";
 import cartRoutes from "./routes/cart.routes.mjs";
 import orderRoutes from "./routes/order.routes.mjs";
 import cors from "cors";
+import path from "path";
+
 const app = express();
 app.use(
-    cors({
-        origin: "http://localhost:5173", // Allow this frontend origin
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials: true, // Allow credentials if needed
-    })
+	cors({
+		origin: "http://localhost:5173", // Allow this frontend origin
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true, // Allow credentials if needed
+	})
 );
 // Middleware
 app.use(bodyParser.json());
@@ -27,6 +29,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
+
+// Serve static files from uploads folder
+const uploadsDir = path.resolve("uploads"); // Resolve the absolute path
+app.use("/api/uploads", express.static(uploadsDir));
 
 export { app }; // Export the app for use in index.js
 // export default app;  // Export the app for use in index.js
