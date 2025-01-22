df175fa330dff125ad301c6a219a18a94f500182
Dev Bachani <dev22603@gmail.com>
2025-01-22 10:50:11 +0530

    Added some validations

--- Backend/controllers/product.controller.mjs
+++ Backend/controllers/product.controller.mjs
@@ -59,6 +59,80 @@ const createProduct = async (req, res) => {
             const category_id = parseInt(req.body.category_id?.trim(), 10); // Convert to integer after trimming
             const stock = parseInt(req.body.stock?.trim(), 10); // Convert to integer after trimming
 
+            if (!product_name) {
+                return res
+                    .status(400)
+                    .json({ message: "Product name is required." });
+            }
+            if (!ws_code) {
+                return res
+                    .status(400)
+                    .json({ message: "WS Code is required." });
+            }
+            if (ws_code <= 0) {
+                return res.status(400).json({
+                    message: "WS Code cannot be less than or equal to 0.",
+                });
+            }
+            if (sales_price <= 0) {
+                return res.status(400).json({
+                    message: "Sales Price cannot be less than or equal to 0.",
+                });
+            }
+            if (mrp <= 0) {
+                return res.status(400).json({
+                    message: "MRP cannot be less than or equal to 0.",
+                });
+            }
+            if (package_size <= 0) {
+                return res.status(400).json({
+                    message: "Package Size cannot be less than or equal to 0.",
+                });
+            }
+            if (category_id <= 0) {
+                return res.status(400).json({
+                    message: "Category ID cannot be less than or equal to 0.",
+                });
+            }
+            const categoryExists = await pool.query(
+                "SELECT * FROM Categories WHERE id = $1",
+                [category_id]
+            );
+            if (categoryExists.rows.length == 0) {
+                return res
+                    .status(400)
+                    .json({ error: "Category does not exist" });
+            }
+            if (!sales_price) {
+                return res
+                    .status(400)
+                    .json({ message: "Sales Price is required." });
+            }
+            if (!mrp) {
+                return res.status(400).json({ message: "MRP is required." });
+            }
+            if (!package_size) {
+                return res
+                    .status(400)
+                    .json({ message: "Package Size is required." });
+            }
+            if (!category_id) {
+                return res
+                    .status(400)
+                    .json({ message: "Category ID is required." });
+            }
+            if (!stock) {
+                return res.status(400).json({ message: "Stock is required." });
+            }
+            if (stock < 0) {
+                return res
+                    .status(400)
+                    .json({ message: "Stock cannot be less than 0." });
+            }
+            if (tags.length === 0) {
+                return res.status(400).json({ message: "Tags are required." });
+            }
+
             const imageURLs = req.files
                 ? req.files.map((file) => `/uploads/${file.filename}`)
                 : [];
@@ -183,7 +257,7 @@ const getProductsByWsCode = async (req, res) => {
 const getProductsByCategory = async (req, res) => {
     const { category_id } = req.params;
     console.log(category_id);
-    
+
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const offset = (page - 1) * limit;
@@ -221,8 +295,15 @@ const getProductsByCategory = async (req, res) => {
 };
 
 const createCategory = async (req, res) => {
-    const { category_name } = req.body;
+    const { category_name } = req.body.category_name?.trim().toLowerCase();
     try {
+        const categoryExists = await pool.query(
+            "SELECT * FROM Categories WHERE category_name = $1",
+            [category_name]
+        );
+        if (categoryExists.rows.length > 0) {
+            return res.status(400).json({ error: "Category already exists" });
+        }
         const result = await pool.query(
             "INSERT INTO Categories (category_name) VALUES ($1) RETURNING *",
             [category_name]
@@ -319,7 +400,124 @@ const deleteProduct = async (req, res) => {
         res.status(500).json({ error: "Error deleting product" });
     }
 };
+// NEW
+// const updateProduct = async (req, res) => {
+//     const { id } = req.params;
+//     console.log(id);
 
+//     upload(req, res, async (err) => {
+//         if (err) {
+//             console.error(err);
+//             return res.status(500).json({ message: "Error uploading images." });
+//         }
+
+//         try {
+//             const product_name = req.body.product_name?.trim();
+//             const ws_code = parseInt(req.body.ws_code?.trim(), 10); // Convert to integer after trimming
+//             const sales_price = parseInt(req.body.sales_price?.trim(), 10); // Convert to integer after trimming
+//             const mrp = parseInt(req.body.mrp?.trim(), 10); // Convert to integer after trimming
+//             const package_size = parseInt(req.body.package_size?.trim(), 10); // Convert to integer after trimming
+//             const tags = req.body.tags.split(",").map((tag) => tag.trim());
+//             const category_id = parseInt(req.body.category_id?.trim(), 10); // Convert to integer after trimming
+//             const stock = parseInt(req.body.stock?.trim(), 10); // Convert to integer after trimming
+//             const existingImages = req.body.existingImages?.trim();
+
+//             if (!product_name) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "Product name is required." });
+//             }
+//             if (!ws_code) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "WS Code is required." });
+//             }
+//             if (!sales_price) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "Sales Price is required." });
+//             }
+//             if (!mrp) {
+//                 return res.status(400).json({ message: "MRP is required." });
+//             }
+//             if (!package_size) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "Package Size is required." });
+//             }
+//             if (!category_id) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "Category ID is required." });
+//             }
+//             if (!stock) {
+//                 return res.status(400).json({ message: "Stock is required." });
+//             }
+//             if (tags.length === 0) {
+//                 return res.status(400).json({ message: "Tags are required." });
+//             }
+
+//             const imageURLs = req.files
+//                 ? req.files.map((file) => `/uploads/${file.filename}`)
+//                 : [];
+
+//             if (imageURLs.length === 0) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "At least one image is required." });
+//             }
+
+//             const images = req.files
+//                 ? req.files.map((file) => `/uploads/${file.filename}`)
+//                 : [];
+//             console.log(existingImages);
+
+//             if (images.length === 0) {
+//                 return res
+//                     .status(400)
+//                     .json({ message: "At least one image is required." });
+//             }
+
+//             console.log([
+//                 product_name,
+//                 ws_code,
+//                 sales_price,
+//                 mrp,
+//                 package_size,
+//                 images,
+//                 tags,
+//                 category_id,
+//                 stock,
+//                 id,
+//             ]);
+
+//             const result = await pool.query(
+//                 "UPDATE Products SET product_name = $1, ws_code = $2, sales_price = $3, mrp = $4, package_size = $5, images = $6, tags = $7, category_id = $8, stock = $9 WHERE id = $10 RETURNING *",
+//                 [
+//                     product_name,
+//                     ws_code,
+//                     sales_price,
+//                     mrp,
+//                     package_size,
+//                     images,
+//                     tags,
+//                     category_id,
+//                     stock,
+//                     id,
+//                 ]
+//             );
+
+//             res.status(201).json(result.rows[0]);
+//         } catch (err) {
+//             console.error(err);
+//             res.status(500).json({
+//                 message: "Error creating product",
+//                 error: err,
+//             });
+//         }
+//     });
+// };
+// OLD
 const updateProduct = async (req, res) => {
     const { id } = req.params;
     console.log(id);
@@ -405,5 +603,5 @@ export {
     getProductsByName,
     getProductsByWsCode,
     getCategories,
-    getProductsByCategory
+    getProductsByCategory,
 };
