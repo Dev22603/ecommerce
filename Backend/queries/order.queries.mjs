df8e3032d40649d9fd119377ad4ccb218a5c635f
Dev Bachani <dev22603@gmail.com>
2025-05-28 11:07:00 +0530

    order query updated

--- Backend/queries/order.queries.mjs
+++ Backend/queries/order.queries.mjs
@@ -1,5 +1,5 @@
 const INSERT_ORDER = `  
-    INSERT INTO Orders (user_id, total_amount) VALUES ($1, $2) RETURNING id;
+    INSERT INTO Orders (user_id, total_amount, address_id) VALUES ($1, $2, $3) RETURNING id;
 `;
 
 export { INSERT_ORDER };
