2e3df36110014e54deaf8392c2dfd7579b5bf47e
Dev Bachani <dev22603@gmail.com>
2025-05-31 01:23:47 +0530

    cart fixed

--- Backend/routes/cart.routes.mjs
+++ Backend/routes/cart.routes.mjs
@@ -1,12 +1,11 @@
 // routes/cart.routes.mjs
 import express from "express";
 import {
-    addItemToCart,
-    checkCartItemQuantity,
-    clearCart,
-    getCart,
-    removeItemFromCart,
-    updateCart,
+	addItemToCart,
+	clearCart,
+	getCart,
+	removeItemFromCart,
+	updateCart,
 } from "../controllers/cart.controllers.mjs";
 import { authenticate } from "../middlewares/auth.mjs";
 import { validateCartUpdate } from "../middlewares/validate_cart.mjs";
@@ -18,7 +17,6 @@ router.post("/add", authenticate, addItemToCart);
 router.get("/", authenticate, getCart);
 router.put("/update", authenticate, validateCartUpdate, updateCart);
 router.delete("/remove/:product_id", authenticate, removeItemFromCart);
-router.post("/check-quantity", authenticate, checkCartItemQuantity); // Route to check the quantity of a specific product in the user's cart
 router.delete("/clear", authenticate, clearCart); // Route to clear all items from the user's cart
 
 export default router;
