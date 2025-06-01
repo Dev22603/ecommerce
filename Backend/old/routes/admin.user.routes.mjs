b5d1f396c37c73defa27766dd7bde4c8eb63f5e2
Dev Bachani <dev22603@gmail.com>
2025-06-01 17:24:05 +0530

    final cleanup

--- Backend/old/routes/admin.user.routes.mjs
+++
@@ -1,9 +0,0 @@
-// routes/admin.user.routes.mjs
-import express from "express";
-import { getAllUsers } from '../controllers/user.controllers.mjs';
-
-const router = express.Router();
-router.get('/users', getAllUsers);
-
-export default router;
-
